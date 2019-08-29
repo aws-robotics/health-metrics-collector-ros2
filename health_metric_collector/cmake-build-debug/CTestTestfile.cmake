473485d57d55d34534c89cdbd0879bfd9297aa74
Tim Robinson <tim@amazon.com>
2019-08-28 17:59:05 -0700

    Remove cmake-build-debug folder

--- health_metric_collector/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cevans/health_metrics/src/health-metrics-collector-ros2/health_metric_collector
-# Build directory: /home/cevans/health_metrics/src/health-metrics-collector-ros2/health_metric_collector/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
