289b669571e4823893bfbfd2ec21979de139a461
Tim Robinson <tim@amazon.com>
2019-08-28 17:54:34 -0700

    Rename package, make launch file ROS1 compatible
    - Rename package to health_metric_collector, removing the _node suffix
    to keep this package consistent with all our others (where github repo,
    folder name and package name are all the same).
    - Improve launch file to be the same as ROS1 where users can override
    node_name or config_file.

--- health_metric_collector/launch/health_metric_collector.launch.py
+++ health_metric_collector/launch/health_metric_collector.launch.py
@@ -1,14 +1,3 @@
-#<launch>
-#    <!-- If a node_name argument is provided by the caller then we will set the node's name to that value -->
-#    <arg name="node_name" default="health_metric_collector" />
-#    <!-- If a config file argument is provided by the caller then we will load it into the node's namespace -->
-#    <arg name="config_file" default="" />
-#
-#    <node name="$(arg node_name)" pkg="health_metric_collector" type="health_metric_collector" respawn="true">
-#        <!-- If the caller specified a config file then load it here. -->
-#        <rosparam if="$(eval config_file!='')" command="load" file="$(arg config_file)"/>
-#    </node>
-#</launch>
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
@@ -22,22 +11,32 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-"""Launch a lifecycle lex node"""
-
 import os
 
+import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # TODO(wjwwood): Use a substitution to find share directory once this is implemented in launch
-    parameters_file_path = os.path.join(get_package_share_directory('health_metric_collector_node'),
+    default_config = os.path.join(get_package_share_directory('health_metric_collector'),
                                         'config', 'sample_configuration.yaml')
+    parameters = [launch.substitutions.LaunchConfiguration("config_file")]
     return LaunchDescription([
-        Node(package='health_metric_collector_node',
-             node_executable='health_metric_collector_node',
-             parameters=[parameters_file_path],
-             output='screen'),
+        launch.actions.DeclareLaunchArgument(
+            "node_name",
+            default_value="health_metric_collector",
+        ),
+        launch.actions.DeclareLaunchArgument(
+            "config_file",
+            default_value=default_config
+        ),
+        Node(
+            package='health_metric_collector',
+            node_executable='health_metric_collector',
+            node_name=launch.substitutions.LaunchConfiguration('node_name'),
+            parameters=parameters,
+            output='screen'
+        ),
     ])
