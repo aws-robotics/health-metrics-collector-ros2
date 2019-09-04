63515cf4756aa2d839eb1040c2f16d5998d44b73
Avishay Alon <avishaya@amazon.com>
2019-09-04 03:14:19 +0000

    Add unit tests (equivalent to the ROS1 version)

--- health_metric_collector/test/health_metric_collector_test.cpp
+++ health_metric_collector/test/health_metric_collector_test.cpp
@@ -19,12 +19,87 @@
 #include <health_metric_collector/metric_collector.h>
 #include <health_metric_collector/metric_manager.h>
 #include <health_metric_collector/sys_info_collector.h>
+#include <ros_monitoring_msgs/msg/metric_data.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <vector>
 
-TEST(CollectorSuite, empty)
+using namespace ros_monitoring_msgs::msg;
+using ::testing::Return;
+
+class MockMetricManager : public MetricManagerInterface
 {
+public:
+  MOCK_METHOD0(Publish, void());
+
+  MOCK_METHOD2(AddDimension, void(const std::string &, const std::string &));
+
+  MOCK_CONST_METHOD0(CreateMetric, MetricData());
+
+  MOCK_METHOD1(AddMetric, void(MetricData));
+};
+
+class MockMetricCollector : public MetricCollectorInterface
+{
+public:
+  MockMetricCollector(std::shared_ptr<MetricManagerInterface> m) : MetricCollectorInterface(m) {}
+  MOCK_METHOD0(Collect, void());
+};
+
+
+TEST(CollectorSuite, Child)
+{
+  auto mg = std::make_shared<MockMetricManager>();
+
+  std::vector<std::shared_ptr<MetricCollectorInterface>> collectors;
+  auto mc = std::make_shared<MockMetricCollector>(mg);
+  collectors.push_back(mc);
+
+  // start metrics collection
+  CollectAndPublish f(mg, collectors);
+
+  MetricData md;
+  ON_CALL(*mg, CreateMetric()).WillByDefault(Return(md));
+  EXPECT_CALL(*mg, Publish()).Times(1);
+  EXPECT_CALL(*mc, Collect()).Times(1);
+
+  f.Publish();
+}
+
+TEST(CollectorSuite, sysinfo)
+{
+  auto mg = std::make_shared<MockMetricManager>();
+  MetricData md;
+  ON_CALL(*mg, CreateMetric()).WillByDefault(Return(md));
+
+  EXPECT_CALL(*mg, AddMetric(testing::_)).Times(4);
+  SysInfoCollector sys_collector(mg);
+  sys_collector.Collect();
+}
+
+TEST(CollectorSuite, cpu_usage_0)
+{
+  auto mg = std::make_shared<MockMetricManager>();
+  MetricData md;
+  ON_CALL(*mg, CreateMetric()).WillByDefault(Return(md));
+
+  EXPECT_CALL(*mg, AddMetric(testing::_)).Times(0);
+
+  CPUMetricCollector cpu_collector(mg);
+  cpu_collector.Collect();
+}
+
+TEST(CollectorSuite, cpu_usage_1)
+{
+  auto mg = std::make_shared<MockMetricManager>();
+  MetricData md;
+  ON_CALL(*mg, CreateMetric()).WillByDefault(Return(md));
+
+  EXPECT_CALL(*mg, AddMetric(testing::_)).Times(testing::AtLeast(1));
+
+  CPUMetricCollector cpu_collector(mg);
+  cpu_collector.Collect();
+  cpu_collector.Collect();
 }
 
 int main(int argc, char ** argv)
